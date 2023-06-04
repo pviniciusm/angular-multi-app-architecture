4f916d59caf09a364a532189de765667ac85a8c8
pviniciusm <pvinics@gmail.com>
2023-06-04 13:16:15 -0300

    default-reset-service now implements reset contract

--- projects/resumo/src/lib/services/default-reset.service.ts
+++ projects/resumo/src/lib/services/default-reset.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { ResetServiceContract } from '../contracts';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class DefaultResetService {
+export class DefaultResetService implements ResetServiceContract {
+  constructor() {}
 
-  constructor() { }
+  public reset(): number {
+    console.log('This is the DEFAULT reset service!');
+
+    return 1;
+  }
 }
