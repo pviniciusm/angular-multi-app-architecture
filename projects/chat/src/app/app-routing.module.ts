3a5189b283c47f9894981b56470e2942ed6b3d92
pviniciusm <pvinics@gmail.com>
2023-06-04 13:04:31 -0300

    use of resumo routes inside chat application with 'resumo' prefix

--- projects/chat/src/app/app-routing.module.ts
+++ projects/chat/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { ResumoRoutingModule } from 'projects/resumo/src/public-api';
 
 const routes: Routes = [
   {
     path: '',
     component: MainPageComponent,
+  },
+  {
+    path: 'resumo',
+    loadChildren: () => ResumoRoutingModule,
   },
 ];
 
