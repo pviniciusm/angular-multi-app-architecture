a879febfb4031393fa98a2c911b22d3a2bff67d9
pviniciusm <pvinics@gmail.com>
2023-06-04 13:16:51 -0300

    resumo main page uses reset method from reset-service contract

--- projects/resumo/src/lib/pages/resumo-page/resumo-page.component.ts
+++ projects/resumo/src/lib/pages/resumo-page/resumo-page.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ResetServiceContract } from '../../contracts';
 
 @Component({
   selector: 'lib-resumo-page',
   templateUrl: './resumo-page.component.html',
-  styleUrls: ['./resumo-page.component.css']
+  styleUrls: ['./resumo-page.component.css'],
 })
 export class ResumoPageComponent implements OnInit {
-
-  constructor() { }
+  constructor(public resetService: ResetServiceContract) {}
 
   ngOnInit(): void {
+    this.resetService.reset();
   }
-
 }
