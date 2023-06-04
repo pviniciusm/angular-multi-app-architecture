e42f7b877fe64b7abaacf0e2d6216848bed7dacc
pviniciusm <pvinics@gmail.com>
2023-06-04 13:08:43 -0300

    use of resumo routes inside cliente application with 'resumo' prefix

--- projects/cliente/src/app/app-routing.module.ts
+++ projects/cliente/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LojaPageComponent } from './pages/loja-page/loja-page.component';
+import { ResumoRoutingModule } from 'projects/resumo/src/public-api';
 
 const routes: Routes = [
   {
     path: '',
     component: LojaPageComponent,
+  },
+  {
+    path: 'resumo',
+    loadChildren: () => ResumoRoutingModule,
   },
 ];
 
